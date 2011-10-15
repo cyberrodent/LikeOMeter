4d527f51cacc46b045fa1c0223d751997d30b7bc
CyberRodent <fej.reblok@gmail.com>
2011-10-15 10:56:37 -0400

    don't store empty fetches in local cache

--- lib/Model.php
+++ lib/Model.php
@@ -9,21 +9,24 @@ require "lib/_data.php";
 
 	function Modelize($token, $id, $meta_data = false) {
 
+		$fetched = 0;
+
 		if ($meta_data) {
 			$qs = "?metadata=1&access_token=$token";
 		} else { 
 			$qs = "?access_token=$token";
 		}
 
-		// cache doesn't know about metadata
+		// cache doesn't know about metadata option
 		$dp = new DataPoint();
 		if ($data = $dp->getFromCache($token, $id)) {
 			return $data;
 		}
 
 		$data = FBUtils::fetchFromFBGraph($id.$qs, $token);
-
-		$dp->storeToCache($token, $id, $data);
+		if ($data) { 
+			$dp->storeToCache($token, $id, $data);
+		}   // else log an error? 
 
 		return (object)$data;
 	}
