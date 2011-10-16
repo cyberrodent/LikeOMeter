d0d11ca220cb0ba2b8caec89f1fd99d5b3b6c4b3
CyberRodent <fej.reblok@gmail.com>
2011-10-16 13:22:27 -0400

    new action for what you and your friends like

--- lib/Homepage_Controller.php
+++ lib/Homepage_Controller.php
@@ -2,6 +2,12 @@
 /**
  * Homepage Controller
  */
+
+
+	function compareLikeCounts(stdClass $like1, stdClass $like2) {
+		return ($like1->count > $like2->count) ? 1 : -1;
+	}
+
 class Homepage_Controller extends Controller {
 
 	/**
@@ -26,14 +32,44 @@ class Homepage_Controller extends Controller {
 		return $set;
 	}
 
+	public function youAndYourFriendsLike($req, $res) {
+
+		$likes = array();
+
+		$res->template = "flikes";
+		// this is the list of friends
+		$friends = Modelize($req->token, "me/friends",null);
+		if (isset($friends->data) and count($friends->data)) { 
+			foreach ($friends->data as $friend) {
+				if (array_key_exists('id', (array)$friend)) { 
+					$likes[$friend->id]      = $this->getAtr("likes", $friend->id, $req->token);
+				}
+			}
+		}
+		$likes['me'] = $this->getAtr("likes", 'me', $req->token);
+
+		$liked = $this->collateLikes( $likes );
+
+		usort($liked, "compareLikeCounts");
+		$liked = array_reverse($liked);
+		return (Object)array(
+			'name' => 'Jeff',
+			'token' => $req->token,
+			'friends' => isset($friends->data) ? $friends->data : null,
+			'liked' => $liked,
+		);
+
+	}
 	public function Hello($req, $res) {
 
 		$likes = array();
 		$musics = array();
 		$books = array();
 		$television = array();
+		$movies = array();
 		$posts = array();
 
+		// for now just a count of all their graph da ta
 		$scores = array();
 
 		$res->template = "newhp";
@@ -57,7 +93,7 @@ class Homepage_Controller extends Controller {
 					// calculate some sort of score for this friend...
 					$score = $this->calculateScore(
 						array(
-//							$core[$friend->id],
+							//	$core[$friend->id],
 							$likes[$friend->id],
 							$books[$friend->id],
 							$movies[$friend->id],
@@ -75,12 +111,12 @@ class Homepage_Controller extends Controller {
 		// FIXME : using 'me' could ufck pu the cache - 2 different me
 		$core['me'] = $this->getAtr('', 'me', $req->token);  
 		$likes['me'] = $this->getAtr("likes", 'me', $req->token);
+
+
 		$books['me'] = $this->getAtr("books", 'me', $req->token);
 		$movies['me'] = $this->getAtr("movies", 'me', $req->token);
 		$musics['me'] = $this->getAtr("music", 'me', $req->token);
 		$television['me'] = $this->getAtr("television", 'me', $req->token);
-
-
 
 
 		return (Object)array(
@@ -95,6 +131,7 @@ class Homepage_Controller extends Controller {
 			'core' => $core,
 			'television' => $television,
 			'scores' => $scores,
+		
 		);
 	}
 	public function calculateScore($stuff) {
@@ -105,5 +142,58 @@ class Homepage_Controller extends Controller {
 			$score += count($att[$key[0]]);
 		}
 		return $score;
+	}
+
+
+
+	/**
+	 * collateLikes 
+	 * 
+	 * @param mixed $all_likes 
+	 * @return void
+	 */
+	public function collateLikes($all_likes) {
+		$liked = array(); // an array of stdClass objs tracking our friend's like
+
+		// the idea here was to find the most liked as we go ... on hold for now
+		$head = new StdClass();
+		$head->id = 0; // the id of the thing liked
+		$head->likers = array(); // users ids who like this
+		$head->count = 0; // count of this->likers
+
+		// the stuff we want is an enigma wrapped in an array wrapped in a twinkie
+		//  and this is how we have to unwrap it.
+		foreach ($all_likes as $uid => $likes) { 
+			foreach ($likes as $aLike) {	
+				foreach ($aLike as $like) {
+					if (!array_key_exists($like->id, $liked)) {
+						$myO = new stdClass();
+						$myO->id = $like->id;
+						$myO->category = $like->category;
+						$myO->name = $like->name;
+						if (!property_exists($myO, 'likers')) {
+							$myO->likers = array();
+						}
+						$liked[$like->id] = $myO;
+					} 
+					$myO = $liked[$like->id];
+					array_push($myO->likers, $uid);
+					$myO->count = count($myO->likers);
+					$liked[$like->id] = $myO;
+
+					/* on hold for now: keep track of the most pop
+					 *
+					if (count($liked[$like->id]) >= $head->count) {
+						$head->count = count($liked[$like->id]);
+						// this isn't always going to work FIXME
+							array_push($head->likers, $uid);
+							$head->id = $like->id;
+					}
+					 *
+					 */	
+				}
+			}
+		}
+		return $liked;
 	}
 }
--- lib/urls.php
+++ lib/urls.php
@@ -10,6 +10,7 @@ class Urls {
 	static  $urls = array(
 		'/' => array('Homepage_Controller','Hello'),
 		'/index' => array('Homepage_Controller','Hello'),
+		'/flikes' => array('Homepage_Controller','youAndYourFriendsLike'),
 		'/resource' => array('Homepage_Controller','Show'),
 	);
 }
