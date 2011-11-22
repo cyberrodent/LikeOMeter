b82718010081448c2a1b44e43fe5184a98b0e0c4
Cyberrodent <fej.reblok@gmail.com>
2011-11-21 20:56:51 -0500

    adding privacy and terms

---
+++ likeometer/privacy.php
@@ -0,0 +1,6 @@
+<h1>Like-O-Meter Privacy Policy</h1>
+
+<p>This Like-O-Meter uses the facebook javascript SDK and any of your data is transmitted from facebook to the browser only. None of the fetched data is stored for any other use.</p>
+
+<p>If you have any questions or concerns about Like-O-Meter, please <a href="mailto:fej.reblok+likeometer@gmail.com">Email Me</a></p>
+
---
+++ likeometer/terms.php
@@ -0,0 +1,6 @@
+<h1>Like-O-Meter Terms of Use</h1>
+
+<p>This Like-O-Meter is provided for entertainment purposes only on as as-is basis.<p>
+
+<p>If you have any questions or concerns about Like-O-Meter, please <a href="mailto:fej.reblok+likeometer@gmail.com">Email Me</a></p>
+
