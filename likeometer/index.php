884a8fcbee00974d2a69076a9d577380f59834dd
Cyberrodent <fej.reblok@gmail.com>
2011-12-18 08:56:47 -0500

    live debug 17

--- likeometer/index.php
+++ likeometer/index.php
@@ -55,7 +55,7 @@ if (1) {
 
 
 
-			echo "<script>top.location.href = \"https://www.facebook.com/dialog/oauth?perms=user_likes,friends_likes&client_id=".  
+			echo "<script>top.location.href = \"https://www.facebook.com/dialog/oauth?scope=email,user_birthday,user_likes,friends_likes&client_id=".  
 				$YOUR_APP_ID."&redirect_uri=".$YOUR_CANVAS_PAGE. "\";</script> ";
 			die();
 		}	
