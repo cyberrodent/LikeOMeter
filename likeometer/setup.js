81b01bc7122bce7451bd02c57ea7aaa6a00e7c0e
Cyberrodent <fej.reblok@gmail.com>
2011-12-19 21:29:32 -0500

    upping perms with read_stream

--- likeometer/setup.js
+++ likeometer/setup.js
@@ -9,7 +9,8 @@ FBLogin = function() {
       } else {
         // console.log('User cancelled login or did not fully authorize.');
       }
-    }, {scope: 'email,friends_likes,user_likes' });
+    }, {scope: 'email,friends_likes,user_likes, read_stream',
+	 perms: 'email,friends_likes,user_likes, read_stream' });
 }
 
 
@@ -33,13 +34,13 @@ $(function(){
       LOM = new Likeometer();
 
 			client_id = '251829454859769';
-			perms_needed = 'email,user_birthday,user_likes,friends_likes';
+			perms_needed = 'email,user_birthday,user_likes,friends_likes,read_stream';
 
 			oauth_url = "https://www.facebook.com/dialog/oauth?scope=" + 
 				perms_needed + "&perms=" + perms_needed + "&client_id=" + 
 				client_id + "&redirect_uri=https://apps.facebook.com/like_o_meter/";
 
-			fql_confirm_perms = 'SELECT friends_likes,user_likes FROM permissions WHERE uid=me()';
+			fql_confirm_perms = 'SELECT read_stream,friends_likes,user_likes FROM permissions WHERE uid=me()';
 
 			_to_login = function() {
 					top.location.href=oauth_url;
@@ -49,6 +50,7 @@ $(function(){
 
 				if (resp[0]['friends_likes'] != 1){_to_login();}
 				if (resp[0]['user_likes'] != 1){_to_login();}
+				if (resp[0]['read_stream'] != 1){_to_login();}
 
 				LOM.init(response.authResponse.accessToken, 
 					response.authResponse.userID);
